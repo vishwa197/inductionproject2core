72fa875af055f4b6c19c858e6766687c4b27bb79
vishwa <vishwabalu@gmail.com>
2020-08-26 13:36:42 +0530

    Previous button feature can now be used and values are able to be stored in localStorage

--- site.js
+++ site.js
@@ -57,6 +57,7 @@ const options=document.querySelector(".options");
 const contentbox=document.querySelector(".content-box");
 const resultbox=document.querySelector(".result-box");
 const namebox=document.querySelector(".name");
+const navbarbox=document.querySelector(".nav-bar");
 
 
 let countquestion=0;
@@ -70,29 +71,55 @@ function getquestions(){
         questionsDisplay.push(quiz[i]);
     }
 }
-
+var attempt={};
+var attempted={};
 contentbox.classList.add("hide");
 function setquestions(x=0){
     resultbox.classList.add("hide");
     countquestion+=x;
-    qnumber="Q"+(countquestion+1);
-    questionNumber.innerHTML=qnumber;
+    questionNumber.innerHTML="Q"+(countquestion+1);
+    if(attempt.hasOwnProperty(countquestion)){
+        console.log(countquestion);
+        currentQuestion=attempt[countquestion];
+        questions.innerHTML=currentQuestion.question;
+
+        
+        options.innerHTML=''; /*So that the options wont get stacked*/
+    let m=4;
+    for(let i=0;i<m;i++){   /*creating options for the particular question*/
+        const option=document.createElement("div");
+        option.innerHTML=currentQuestion.options[i];
+        option.id=i;
+        option.className="option";    
+        options.appendChild(option);
+        if(attempted[countquestion]){
+            options.children[i].classList.add("attempted-already");
+        }
+        else{
+            option.setAttribute("onclick","result(this)");
+        }
+    }
+   
+}
+    else{
     currentQuestion=questionsDisplay[Math.floor(Math.random()*questionsDisplay.length)];
+    attempt[countquestion]=currentQuestion;
+    console.log(attempt);
     questions.innerHTML=currentQuestion.question;
     const index=questionsDisplay.indexOf(currentQuestion);
     questionsDisplay.splice(index,1)
     
 
     options.innerHTML=''; /*So that the options wont get stacked*/
-    let m=4,y=0;
-    for(let i=0;i<m;i++){   /*creating options for the particular question*/
+    let n=4;
+    for(let i=0;i<n;i++){   /*creating options for the particular question*/
         const option=document.createElement("div");
         option.innerHTML=currentQuestion.options[i];
         option.id=i;
         option.className="option";    
         options.appendChild(option); /*Adding newly created option container into the options container*/       
         option.setAttribute("onclick","result(this)");
-    }
+    }}
     
 }
 
@@ -112,6 +139,9 @@ function result(answered){
                 }
             }
     }
+    attempted[countquestion]=true;
+    localStorage.setItem("correct",correct);
+    localStorage.setItem("wrong",wrong);
     answerlimit();
 }
 function answerlimit(){
@@ -120,29 +150,31 @@ function answerlimit(){
         options.children[i].classList.add("attempted-already");
     }
 }
-
+var o=0;
+var timeLeft=100;
 function startquiz(){
     contentbox.classList.remove("hide");
     namebox.classList.add("hide");
     welcome=document.getElementById("heading");
     welcome.innerHTML="Welcome to the COVID-19 Quiz, "+name;
-    var timeLeft = 100;
     time = document.getElementById('countdown');
     var timerId = setInterval(countdown, 1000);
     function countdown() {
-      if (timeLeft == -1) {
+      if (timeLeft == -100000 ) {
         clearTimeout(timerId);
         quizover();
-      } else {
+      } else if(o!=1){
         time.innerHTML = ' Time remaining: '+timeLeft;
         timeLeft--;
       }
     }
 }
 function writename(){
-    name =document.getElementById("quiz").value;   
+    name =document.getElementById("quiz").value;
+    localStorage.setItem("name",name);   
 }
 writename();
+
 function quizresult(){
     resultbox.querySelector(".name").innerHTML=name;
     resultbox.querySelector(".totalquestions").innerHTML=quiz.length;
@@ -153,14 +185,26 @@ function quizresult(){
     resultbox.querySelector(".scorepercentage").innerHTML=(correct/(quiz.length))*100+"%";
 }
 function quizover(){
+    o++;
+    localStorage.setItem("timeLeft",timeLeft);
     resultbox.classList.remove("hide");
     contentbox.classList.add("hide");
-    time.innerHTML="Time Out!";
+    console.log(timeLeft);
+    if(timeLeft==-1){
+        time.innerHTML="Time Out!";
+    }
     quizresult();
 }
+function navbar(){
+    const totalquestions=quiz.length;
+    for(let i=0;i<totalquestions;i++){
+        const nav=document.createElement("div");
+        navbarbox.appendChild(nav);
+    }
+}
+
 function next(){
     if(countquestion+1==10){
-        console.log("Quiz over");
         quizover();
     }
     else{
@@ -175,8 +219,42 @@ function previous(){
       console.log("Quiz just started");
    }
 }
+c=parseInt(localStorage.getItem("correct"));
+w=parseInt(localStorage.getItem("wrong"));
+t=parseInt(localStorage.getItem("timeLeft"));
+console.log(c);
+var score1=0;
+var score2=0;
+    switch(c-w){
+        case 10:
+        score1=9.5;break;    
+        case 9:
+        case 8:
+        score1=8.5;break;
+        case 7:
+        case 6:
+        score1=6.5;break;
+        case 5:
+        case 4:
+        score1=5;break;
+        case 3:
+        case 2:
+        score1=3;break;
+        case 1:
+        score1=2;break;
+        default:
+            score1=0;            
+    }
+score2=(score1/(Math.sqrt(t)))*10;
+console.log(score2);    
+window.localStorage.time = new Date().getTime();
+var date = new Date(parseInt(window.localStorage.time));
+console.log(date);
 
 window.onload=function(){
     getquestions();
     setquestions();
+    navbar();
 }
+console.log(localStorage);
+
